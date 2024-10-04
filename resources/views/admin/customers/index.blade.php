e2c158ea1dce6ab1df9f19e687cfa38d418e8d6d
lipikhin <a.lipikhin@gmail.com>
2024-10-04 13:02:19 -0400

    upd customers

--- resources/views/admin/customers/index.blade.php
+++ resources/views/admin/customers/index.blade.php
@@ -15,9 +15,6 @@
         /*}*/
     </style>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css">
-
     <div class="container" style="max-width: 550px;">
         <div class="card shadow">
             <div class="card-header">
@@ -73,9 +70,4 @@
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    {{--    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>--}}
-    <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
-
 @endsection
